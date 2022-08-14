d8cf50a8a08b6bae8739f50c7a20c6663cf1966c
RainerHeintzmann <heintzmann@gmail.com>
2022-08-14 16:05:34 +0200

    change in example

--- examples/distille_psfs.jl
+++ examples/distille_psfs.jl
@@ -3,30 +3,40 @@ using PointSpreadFunctions
 using NDTools
 using FourierTools
 using InverseModeling
+using Noise
 
 sz = (256,256,100)
-sampling = (0.040,0.040,0.100)
+sampling = (0.020,0.020,0.050)
 # simulate a confocal PSF
 pp_em = PSFParams(0.5,1.4,1.52; mode=ModeConfocal);
-pp_ex = PSFParams(pp_em; λ=0.488, aplanatic=aplanatic_illumination);
-p_conf = psf(sz,pp_em; pp_ex=pp_ex, pinhole=0.1, sampling=sampling);
 
-beads = zeros(sz)
-N = 10
+# aberrations = Aberrations();
+aberrations = Aberrations([Zernike_HorizontalComa,Zernike_Tip],[0.2,0.2]);
+pp_ex = PSFParams(pp_em; λ=0.488, method=MethodPropagateIterative, aplanatic=aplanatic_illumination, aberrations=aberrations);
+p_conf = psf(sz,pp_em; pp_ex=pp_ex, pinhole=1.5, sampling=sampling);
+params_c, fwd_c, optim_res = PSFDistiller.gauss_fit(p_conf)
+@ve fwd_c p_conf  p_conf
+
+n_photons = 10000
+p_conf = n_photons .* p_conf ./ maximum(p_conf)
+
+# create random delta peaks
+beads = zeros(sz); N = 10
 beads[rand(1:prod(sz),N)] .= 1.0
 
 perfect_img = conv_psf(beads, p_conf)
+n_img = poisson(perfect_img)
 
 apsf, rois, positions, selected, params, fwd = distille_PSF(perfect_img)
 params2, fwd2, allp2 = PSFDistiller.gauss_fit(apsf)
 
-sampling .* params[:FWHM]
-sampling .* params2[:FWHM]
-params2[:R2]
-@vt fwd2 apsf (fwd2 .- apsf)
-mpsf = sum(apsf)/prod(size(apsf))
+@show sampling .* params[:FWHM]
+@show sampling .* params2[:FWHM]
+@show params2[:R2]
+# @vt fwd2 apsf (fwd2 .- apsf)
+@show mpsf = sum(apsf)/prod(size(apsf))
 
-1.0 - sum(abs2.(fwd2 .- apsf)) / sum(abs2.(apsf .- mpsf))
+@show 1.0 - sum(abs2.(fwd2 .- apsf)) / sum(abs2.(apsf .- mpsf))
 
 # visualize the result using View5D.jl
-# @ve perfect_img selected selected
+# @ve n_img selected 
